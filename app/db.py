57ac694081f3c58382f623e7f1e83898692b116f
Ilya G <ilya.grigoryan@11gmail.com>
2024-11-23 22:56:48 +0300

    works!

--- app/db.py
+++ app/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 # URL подключения к базе данных
 DATABASE_URL = "postgresql+asyncpg://postgres@localhost:5432/kanban_db"
